ee538b164ec386b55932a435aa9a5369517a8b44
ahmdyudaa <ahmadyuda.hermawan05@gmail.com>
2024-03-06 21:54:10 +0700

    add asset and konten

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Portfolio',
+      title: 'Ahmad Yuda Hermawan/123210075',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Color.fromARGB(255, 82, 57, 27)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 20, 105, 133)),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -28,52 +28,64 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('My Portfolio', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary ),),
+          title: Text(
+            'King Invitation',
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          ),
           backgroundColor: Theme.of(context).colorScheme.primary,
         ),
-        body: SingleChildScrollView(
+        body: const SingleChildScrollView(
             child: Column(
           children: [
             Text("Ahmad Yuda Hermawan"),
             Text("123210075"),
-            BlogItem(
-              blogTitle: "Blog Pertama",
-              blogDescription: "Ini adalah blog pertama saya",
+            CatalogItem(
+              catalogTitle: "Elegant 1",
+              catalogDescription: "Ini adalah tema elegant 1",
+              imageSource: 'images/elegant1.jpg',
             ),
-            BlogItem(
-              blogTitle: "Blog Kedua",
-              blogDescription: "Ini adalah blog kedua saya",
+            CatalogItem(
+              catalogTitle: "Elegant 2",
+              catalogDescription: "Ini adalah tema elegant 2",
+              imageSource: 'images/elegant2.jpg',
             ),
-            BlogItem(
-              blogTitle: "Blog Kedua",
-              blogDescription: "Ini adalah blog kedua saya",
+            CatalogItem(
+              catalogTitle: "Elegant 3",
+              catalogDescription: "Ini adalah tema elegant 3",
+              imageSource: 'images/elegant3.jpg',
             ),
-            BlogItem(
-              blogTitle: "Blog Kedua",
-              blogDescription: "Ini adalah blog kedua saya",
+            CatalogItem(
+              catalogTitle: "Elegant 4",
+              catalogDescription: "Ini adalah tema elegant 4",
+              imageSource: 'images/elegant4.png',
             ),
-            BlogItem(
-              blogTitle: "Blog Kedua",
-              blogDescription: "Ini adalah blog kedua saya",
+            CatalogItem(
+              catalogTitle: "Elegant 5",
+              catalogDescription: "Ini adalah tema elegant 5",
+              imageSource: 'images/elegant5.png',
             ),
-            BlogItem(
-              blogTitle: "Blog Kedua",
-              blogDescription: "Ini adalah blog kedua saya",
+            CatalogItem(
+              catalogTitle: "Elegant 6",
+              catalogDescription: "Ini adalah tema elegant 6",
+              imageSource: 'images/elegant6.jpg',
             ),
+            ContactForm(),
           ],
         )));
   }
 }
 
-class BlogItem extends StatelessWidget {
-  const BlogItem({
-    super.key,
-    required this.blogTitle,
-    required this.blogDescription,
-  });
+class CatalogItem extends StatelessWidget {
+  const CatalogItem({
+    Key? key,
+    required this.catalogTitle,
+    required this.catalogDescription,
+    required this.imageSource,
+  }) : super(key: key);
 
-  final String blogTitle;
-  final String blogDescription;
+  final String catalogTitle;
+  final String catalogDescription;
+  final String imageSource;
 
   @override
   Widget build(BuildContext context) {
@@ -81,68 +93,163 @@ class BlogItem extends StatelessWidget {
       padding: const EdgeInsets.all(12),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.grey[200],
+          color: Theme.of(context).colorScheme.surfaceVariant,
           borderRadius: BorderRadius.circular(8),
         ),
         child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: const Image(
-                    image: AssetImage('images/gambar1.jpg'),
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image(
+                    image: AssetImage(imageSource),
                     fit: BoxFit.cover,
                   ),
                 ),
-                Expanded(
-                    flex: 2,
-                    child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+              FittedBox(
+                child: Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          catalogTitle,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(catalogDescription),
+                        const SizedBox(height: 10),
+                        Row(
                           children: [
-                            Text(
-                              blogTitle,
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ElevatedButton(
+                              onPressed: () {
+                                print('Tombol Order Now diklik');
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.primary,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                "Order Now",
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                ),
+                              ),
                             ),
-                            SizedBox(height: 5),
-                            Text(blogDescription),
-                            SizedBox(height: 10),
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                    onPressed: null,
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.blue,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(
-                                            10), 
-                                      ),
-                                      
-                                    ),
-                                    child: Text("Order Now")),
-                                    SizedBox(width: 5),
-                                OutlinedButton(
-                                    onPressed: null,
-                                    style: OutlinedButton.styleFrom(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(
-                                            10), 
-                                            
-                                      ),
-                                      
-                                    ),
-                                    child: Text("Chat Admin"))
-                              ],
+                            const SizedBox(width: 5),
+                            OutlinedButton(
+                              onPressed: () {
+                                print('Tombol Chat Admin diklik');
+                              },
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .secondaryContainer,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                "Chat Admin",
+                                style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondaryContainer,
+                                ),
+                              ),
                             )
                           ],
-                        ))),
-              ],
-            )),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class ContactForm extends StatelessWidget {
+  const ContactForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(12),
+                  child: Center(
+                    child: Text(
+                      "Give Me Your Request",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Message',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Tombol Send diklik");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    "Send",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
